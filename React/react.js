b6180205e2a08334d769e3c7ebd42d50449d1156
unknown <2116025@saec.ac.in>
2024-06-14 20:48:47 +0530

    Assign_17

---
+++ React/react.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const hele = React.createElement("h1", null, "Hello");
+
+const root = ReactDOM.createRoot(document.getElementById("fde"));
+
+root.render(hele);
